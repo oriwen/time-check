c680d10f8f8a7ad95909863c45dc2f357428e65b
oriwen <a.slivka@seznam.cz>
2015-11-05 08:25:51 +0100

    Update time-gui.py

--- time-gui.py
+++ time-gui.py
@@ -79,7 +79,7 @@ class TimeWindow(tk.Tk):
 
         self.frames = {}
         
-        for F in (MainWindow, SetupWindow):
+        for F in (MainWindow, SetupWindow, LoginWindow):
         
             frame = F(container, self)
         
@@ -351,7 +351,7 @@ class MainWindow(tk.Frame):
         button4 = tk.Button(self, text="Save", fg="red", command=savetime)
         button4.grid(row=1, column=7)
         
-        button5 = tk.Button(self, text="Login", fg="red", command=popupmsg)
+        button5 = tk.Button(self, text="Login", fg="red", command=lambda: controller.show_frame(LoginWindow))
         button5.grid(row=2, column=7)
         
         button6 = tk.Button(self, text="About", fg="red", command=popupabout)
@@ -590,6 +590,426 @@ class SetupWindow(tk.Frame):
         labelc1.configure(bg=color1_temp)
         labelc2.configure(bg=color2_temp)
         
+class LoginWindow(tk.Frame):
+    
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        
+        e1_var = "6:00"
+        e2_var = "8:00"
+        e3_var = "30"
+        
+        ev1 = tk.StringVar()
+        ev2 = tk.StringVar()
+        ev3 = tk.StringVar()
+        
+        ev1.set(e1_var)
+        ev2.set(e2_var)
+        ev3.set(e3_var)
+        
+        def Set_Enum_1(self):
+            global enum
+            enum = 1
+    
+        def Set_Enum_2(self):
+            global enum
+            enum = 2
+            
+        def export1(self):
+            global eexp
+            if enum == 1:
+                eexp = str(ev1.get())
+            else:
+                eexp = str(ev2.get())
+                
+        def AddTime1(self):
+            v1 = eexp
+            v1_h, v1_m = v1.split(':') 
+            v1_h1 = int(v1_h)
+            v1_m1 = int(v1_m) + 1
+    
+            if v1_m1 > 59:
+                v2_h = int(v1_h1 + (v1_m1 // 60))
+                v2_m = int(v1_m1 - 60)
+            elif v1_h1 > 22:
+                v2_h = 22
+                v2_m = v1_m1
+            elif v1_h1 < 2:
+                v2_h = 2
+                v2_m = v1_m1
+            else:
+                v2_h = v1_h1
+                v2_m = v1_m1
+  
+            time_add = str((str(dt.timedelta(hours=v2_h, minutes=v2_m)))[:-3])
+    
+            if enum == 1:
+                ev1.set(time_add)
+    
+            else:
+                ev2.set(time_add)
+    
+        def AddTime15(self):
+            v1 = eexp
+            v1_h, v1_m = v1.split(':') 
+            v1_h1 = int(v1_h)
+            v1_m1 = int(v1_m) + 15
+    
+            if v1_m1 > 59:
+                v2_h = int(v1_h1 + (v1_m1 // 60))
+                v2_m = int(v1_m1 - 60)
+            elif v1_h1 > 22:
+                v2_h = 22
+                v2_m = v1_m1
+            elif v1_h1 < 2:
+                v2_h = 2
+                v2_m = v1_m1
+            else:
+                v2_h = v1_h1
+                v2_m = v1_m1
+        
+            time_add = str((str(dt.timedelta(hours=v2_h, minutes=v2_m)))[:-3])
+        
+            if enum == 1:
+                ev1.set(time_add)
+    
+            else:
+                ev2.set(time_add)
+    
+        def SubTime1(self):
+            v1 = eexp
+            v1_h, v1_m = v1.split(':') 
+            v1_h1 = int(v1_h)
+            v1_m1 = int(v1_m) - 1
+    
+            if v1_m1 < 0:
+                v1_m1 = 59
+                v2_h = int(v1_h1 - 1)
+                v2_m = int(v1_m1)
+            elif v1_h1 > 22:
+                v2_h = 22
+                v2_m = v1_m1
+            elif v1_h1 < 2:
+                v2_h = 2
+                v2_m = v1_m1
+            else:
+                v2_h = v1_h1
+                v2_m = v1_m1
+        
+            time_add = str((str(dt.timedelta(hours=v2_h, minutes=v2_m)))[:-3])
+        
+            if enum == 1:
+                ev1.set(time_add)
+    
+            else:
+                ev2.set(time_add)
+    
+        def SubTime15(self):
+            v1 = eexp
+            v1_h, v1_m = v1.split(':') 
+            v1_h1 = int(v1_h)
+            v1_m1 = int(v1_m) - 15
+    
+            if v1_m1 < 0:
+                v1_m1 = v1_m1 + 60
+                v2_h = int(v1_h1 - 1)
+                v2_m = int(v1_m1)
+            elif v1_h1 > 22:
+                v2_h = 22
+                v2_m = v1_m1
+            elif v1_h1 < 2:
+                v2_h = 2
+                v2_m = v1_m1
+            else:
+                v2_h = v1_h1
+                v2_m = v1_m1
+        
+            time_add = str((str(dt.timedelta(hours=v2_h, minutes=v2_m)))[:-3])
+        
+            if enum == 1:
+                ev1.set(time_add)
+    
+            else:
+                ev2.set(time_add)
+    
+        def AddTime1m(self):
+            v1 = str(e3.get())
+            v1_m1 = int(v1) + 1
+    
+            if v1_m1 < 59:
+                v2_m = v1_m1
+            else:
+                v2_m = 59
+  
+            time_add = str((str(dt.timedelta(minutes=v2_m)))[2:-3])
+            ev3.set(time_add)
+    
+        def AddTime15m(self):
+            v1 =str(e3.get())
+            v1_m1 = int(v1) + 15
+    
+            if v1_m1 < 59:
+                v2_m = v1_m1
+            else:
+                v2_m = 59
+        
+            time_add = str((str(dt.timedelta(minutes=v2_m)))[2:-3])
+            ev3.set(time_add)
+    
+        def SubTime1m(self):
+            v1 =str(e3.get())
+            v1_m1 = int(v1) - 1
+    
+            if v1_m1 < 30:
+                v2_m = 30
+            else:
+                v2_m = v1_m1
+        
+            time_add = str((str(dt.timedelta(minutes=v2_m)))[2:-3])
+            ev3.set(time_add)
+    
+        def SubTime15m(self):
+            v1 =str(e3.get())
+            v1_m1 = int(v1) - 15
+    
+            if v1_m1 < 30:
+                v2_m = 30
+            else:
+                v2_m = v1_m1
+        
+            time_add = str((str(dt.timedelta(minutes=v2_m)))[2:-3])
+            ev3.set(time_add)
+                
+        def AddTime1c(self):
+            export1(self)
+            AddTime1(self)
+    
+        def AddTime15c(self):
+            export1(self)
+            AddTime15(self)
+    
+        def SubTime1c(self):
+            export1(self)
+            SubTime1(self)
+    
+        def SubTime15c(self):
+            export1(self)
+            SubTime15(self)
+            
+        def show_buttons():
+            button1.grid_forget() 
+            button2.grid(row=0, column=6)  
+            button3.grid(row=0, column=7)
+            button4.grid(row=1, column=7)  
+            button5.grid(row=2, column=7)
+            button6.grid(row=3, column=7)
+              
+        def hide_buttons():
+            button1.grid(row=0, column=6)  
+            button2.grid_forget()   
+            button3.grid_forget()
+            button4.grid_forget()  
+            button5.grid_forget()
+            button6.grid_forget()
+            
+        def savetime():
+            config = configparser.ConfigParser()
+            config.read("config.ini")
+            config.set("preset", "arrival_time", e1.get())
+            config.set("preset", "work_time", e2.get())
+            config.set("preset", "lunch_break", e3.get())
+            with open("config.ini", "w") as configfile:
+                config.write(configfile)
+                
+        def loginaction():
+            l1.grid(row=0, column=0)
+            e1.grid(row=0, column=1, pady=4)
+            l2.grid(row=1, column=0)
+            e2.grid(row=1, column=1, pady=4)
+            l3.grid(row=1, column=4)
+            e3.grid(row=1, column=5)
+            l4.grid(row=1, column=6, padx=2)
+            timeathome.grid(row=2, columnspan=7, sticky="nsew")
+            timeleft.grid(row=3, columnspan=7, sticky="nsew")
+            loginlabel.grid_forget()
+            loginentry.grid_forget()
+            passlabel.grid_forget()
+            passentry.grid_forget()
+            
+        l1 = tk.Label(self, text="You have arrived at: ")
+        
+        e1 = tk.Entry(self)
+        e1["width"] = 5
+        e1.bind("<Enter>", Set_Enum_1 )
+        e1.bind("<Button-1>", AddTime1c )
+        e1.bind("<Shift-1>", AddTime15c )
+        e1.bind("<Button-3>", SubTime1c )
+        e1.bind("<Shift-3>", SubTime15c )
+        e1.config(bg="white", state="readonly", textvariable = ev1)
+        
+        button1 = tk.Button(self, text=">", fg="red", command=show_buttons)
+        button1.grid(row=0, column=6)
+        button1.lower()
+        
+        button2 = tk.Button(self, text="<", fg="red", command=hide_buttons)
+        button2.grid(row=0, column=6)
+
+        button3 = tk.Button(self, text="Setup", fg="red", command=lambda: controller.show_frame(SetupWindow))
+        button3.grid(row=0, column=7)
+        
+        button4 = tk.Button(self, text="Save", fg="red", command=savetime)
+        button4.grid(row=1, column=7)
+        
+        button5 = tk.Button(self, text="Login", fg="red", command=popupmsg)
+        button5.grid(row=2, column=7)
+        
+        button6 = tk.Button(self, text="About", fg="red", command=popupabout)
+        button6.grid(row=3, column=7)
+        
+        l2 = tk.Label(self)
+        l2["text"] = "How long will you work?:"
+    
+        e2 = tk.Entry(self)
+        e2["width"] = 5
+        e2.bind("<Enter>", Set_Enum_2 )
+        e2.bind("<Button-1>", AddTime15c )
+        e2.bind("<Button-3>", SubTime15c )
+        e2.config(bg="white", state="readonly", textvariable = ev2)
+
+        l3 = tk.Label(self)
+        l3["text"] = "And lunch? :"
+
+        e3 = tk.Entry(self)
+        e3["width"] = 2
+        e3.bind("<Button-1>", AddTime1m )
+        e3.bind("<Shift-1>", AddTime15m )
+        e3.bind("<Button-3>", SubTime1m )
+        e3.bind("<Shift-3>", SubTime15m )
+        e3.config(bg="white", state="readonly", textvariable = ev3)
+    
+        l4= tk.Label(self)
+        l4["text"] = "minutes"
+        
+        timeathome = tk.Label(self)
+        timeathome.configure(text="Sample", bg="green", font=("Font",20))
+        
+        timeleft = tk.Label(self)
+        timeleft.configure(text="Sample", bg="yellow", font=("Font",20))
+        
+        loginlabel = tk.Label(self, text="Osobní číslo")
+        loginlabel.grid(row=0, column=1)
+        
+        loginentry = tk.Entry(self)
+        loginentry["width"] = 10
+        loginentry.grid(row=0, column=2)
+        
+        passlabel = tk.Label(self, text="Heslo")
+        passlabel.grid(row=1, column=1)
+        
+        passentry = tk.Entry(self)
+        passentry["width"] = 15
+        passentryl.grid(row=1, column=2)
+        
+        Loginbutton = tk.Button(self, text = "Login", command=loginaction)
+        
+        
+        config = configparser.ConfigParser()
+        config.read("config.ini")
+        ev1_time = (config["preset"]["arrival_time"])
+        ev2_time = (config["preset"]["work_time"])
+        ev3_time = (config["preset"]["Lunch_break"])
+        ev1.set(ev1_time)
+        ev2.set(ev2_time)
+        ev3.set(ev3_time)
+        color1_temp = str((config["visual"]["color1"]))
+        color2_temp = str((config["visual"]["color2"]))
+        timeathome.configure(bg=color1_temp)
+        timeleft.configure(bg=color2_temp)
+        
+        def tick1():
+            now = dt.datetime.now()
+            h1 = str(now.hour)
+            m1 = str(now.minute)
+            s1 = str(now.second)
+
+# arrival time
+            while True:
+                try:
+                    t_in =str(e1.get())
+                    t_in_h, t_in_m = t_in.split(':') 
+                    val1 = int(t_in_h)
+                    val2 = int(t_in_m)
+                    break
+                except (ValueError):
+                    t_in_h = "06"
+                    t_in_m = "00"
+                break
+    
+# work time    
+            while True:
+                try:
+                    t_work = str(e2.get())
+                    t_work_h, t_work_m = t_work.split(':') 
+                    val3 = int(t_work_h)
+                    val4 = int(t_work_m)
+                    break
+                except (ValueError):
+                    t_work_h = "08"
+                    t_work_m = "00"
+                break
+    
+# lunch break
+            while True:
+                try:
+                    t_lunch = e3.get()
+                    break
+                except (ValueError):
+                    t_lunch = "30"
+                break
+    
+#variables preparation
+            t_now = h1 + ":" + m1 + ":" + s1
+            t_ar= t_in_h + ":" + t_in_m + ":00"
+            t_m = dt.timedelta(minutes=int(t_work_m)) + dt.timedelta(minutes=int(t_lunch))
+            t_w_h, t_w_m, t_w_s = str(t_m).split(':')
+            time_working = str(int(t_work_h) + int(t_w_h)) + ":" + str(t_w_m) + ":00"
+            fmt = '%H:%M:%S'
+            tmin = dt.datetime(year=1999, month=1, day=1, hour=0, minute=0, second=0)
+    
+#time values calculation and check
+            time_lost = dt.datetime.strptime(str(t_now), str(fmt)) - dt.datetime.strptime(t_ar, fmt)
+            time_lost1 = dt.datetime.strptime(str(time_working), str(fmt)) - dt.datetime.strptime(str(time_lost), str(fmt))
+            time_home = dt.datetime.strptime(str(t_now), str(fmt)) + time_lost1
+            time_print = dt.datetime.time(time_home)
+            time_total = tmin + time_lost1
+            showtime1 = (time_total).time()
+
+#exceptions handling 
+            while True:
+                    set1 = time_total.year
+                    if set1 < 1999:
+                        print_finished = "Go Home"
+                    else: 
+                        print_finished = showtime1
+                    break
+    
+            timeleft.config(text=str(time_print))
+    
+            if print_finished != timeleft:["text"]
+            timeleft["text"] = print_finished
+            timeleft.after(200, tick1)
+            
+            timeathome.config(text=str(time_print))
+    
+            if print_finished != timeathome:["text"]
+            timeathome["text"] = time_print
+            
+# Must check under windows......
+            
+            if tk.Tk.wm_state(controller) == "iconic":
+                tk.Tk.wm_state(controller, "icon")
+
+        tick1()
         
 app = TimeWindow()
 app.resizable(0,0)
